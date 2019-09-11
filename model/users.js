53e5caeeea62cb690677d830fd571fa3385b7749
Bazgest <r.ratthphong@kkumail.com>
2019-09-11 10:45:54 +0700

    test commit

--- model/users.js
+++ model/users.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const User = new Schema({
+  firstname: String,
+  lastname: String,
+  type: String,
   email: String,
-  password: String
+  password: String,
+  dateofbirth: Date,
+  department: String,
+  position: String,
+  phone: String
 })
 
 module.exports = mongoose.model('users', User)
