78a5885768eb3500dc74597bc70d766614512189
Supakit <supakit_c@cmu.ac.th>
2019-09-11 12:52:59 +0700

    set default value.

--- model/users.js
+++ model/users.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const User = new Schema({
-  firstname: String,
-  lastname: String,
+  firstname: { type: String, default: null },
+  lastname: { type: String, default: null },
   type: String,
   email: String,
-  password: String,
-  dateofbirth: Date,
-  department: String,
-  position: String,
-  phone: String
+  password: { type: String, default: null },
+  dateofbirth: { type: Date, default: null },
+  department: { type: String, default: null },
+  position: { type: String, default: null },
+  phone: { type: String, default: null },
+  created_at: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('users', User)
